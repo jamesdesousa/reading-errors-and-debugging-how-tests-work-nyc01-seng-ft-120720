7d6850aaee7960bf07c488f3c76a51b89c37eb18
James DeSousa <james.desousa45@gmail.com>
2020-11-21 21:02:55 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,7 @@ second_number = 8
 
 sum = first_number + second_number
 
-difference = second_number - first_number
+difference = first_number - first_number
 
 product = first_number * second_number
 
