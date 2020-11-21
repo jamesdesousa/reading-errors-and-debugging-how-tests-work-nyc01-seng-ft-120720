cefe42d8eba2cc2b78e87a28506359d4d59f52de
James DeSousa <james.desousa45@gmail.com>
2020-11-21 21:02:15 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
-# Add your variables here
+first_number = 4
+
+second_number = 8
+
+sum = first_number + second_number
+
+difference = sedond_number - first_number
+
+product = first_number * second_number
+
+quotient = second_number/first_number
+
